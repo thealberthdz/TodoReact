72ec67e8b67a0e4a6a5c71c4644ccebff0e3ef64
Luis Alberto Hernández Ramos <lhernandez@OCCQRO.local>
2017-01-20 12:00:19 -0600

    Creating a Todo list

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,12 +1,37 @@
 var React = require('react');
 
+var TodoList = require('TodoList');
+
 var TodoApp = React.createClass({
+	getInitialState: function(){
+		return {
+			todos: [
+				{
+					id: 1,
+					text: "Walk the dog"
+				},
+				{
+					id: 2,
+					text: "do the dishes"
+				},
+				{
+					id: 3,
+					text: "Do my bed"
+				},
+				{
+					id: 4,
+					text: 'Fuck You!'
+				}
+			]
+		};
+	},
 	render: function(){
+		var {todos} = this.state;
 		return(
 			<div>
-				TodoApp.jsx
+				<TodoList todos={todos} />
 			</div>
-		)
+		);
 	}
 });
 
